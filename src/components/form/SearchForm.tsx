5c6f33bcd88a7f92013297d52f1dd29edea0b159
Diwash <diwaskarki96@gmail.com>
2025-04-06 23:26:07 +0545

    initial commit

---
+++ src/components/form/SearchForm.tsx
@@ -0,0 +1,25 @@
+import React, { FormEvent, useState } from "react";
+type SearchFormProps = {
+  userName: string;
+  setUserName: React.Dispatch<React.SetStateAction<string>>;
+};
+const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
+  const [text, setText] = useState(userName);
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!text) {
+      console.log("Please enter a username");
+    }
+    setUserName(text);
+  };
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="flex items-center gap-x-2 w-full lg:w-1/3 mb-3"
+    >
+      <h1 className="sr-only">hi</h1>
+    </form>
+  );
+};
+
+export default SearchForm;
