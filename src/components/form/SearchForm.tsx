559d5c8ed27ba16933e3fac7e2175facaf6658f9
Diwash <diwaskarki96@gmail.com>
2025-04-07 23:11:55 +0545

    mui

--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,4 +1,5 @@
 import React, { FormEvent, useState } from "react";
+import TextField from "@mui/material/TextField";
 type SearchFormProps = {
   userName: string;
   setUserName: React.Dispatch<React.SetStateAction<string>>;
@@ -18,6 +19,7 @@ const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
       className="flex items-center gap-x-2 w-full lg:w-1/3 mb-3"
     >
       <h1 className="sr-only">hi</h1>
+      <TextField label="Outlined" variant="outlined" value={text} onChange={} />
     </form>
   );
 };
