4822c77ecf25cf9ffe8e9de739a2b7ceb639c4b7
Diwash <diwaskarki96@gmail.com>
2025-04-14 19:56:48 +0545

    toastify

--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,5 +1,8 @@
 import React, { FormEvent, useState } from "react";
 import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+
 type SearchFormProps = {
   userName: string;
   setUserName: React.Dispatch<React.SetStateAction<string>>;
@@ -9,7 +12,7 @@ const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
   const handleSearch = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!text) {
-      console.log("Please enter a username");
+      toast.error("Please enter a username");
     }
     setUserName(text);
   };
@@ -19,7 +22,26 @@ const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
       className="flex items-center gap-x-2 w-full lg:w-1/3 mb-3"
     >
       <h1 className="sr-only">hi</h1>
-      <TextField label="Outlined" variant="outlined" value={text} onChange={} />
+      <TextField
+        placeholder="Search Github Users..."
+        className="flex-grow bg-background"
+        variant="outlined"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <Button
+        sx={{
+          bgcolor: "black",
+          color: "white",
+          paddingX: "1rem",
+          textTransform: "none",
+        }}
+        type="submit"
+      >
+        Search
+      </Button>
     </form>
   );
 };
