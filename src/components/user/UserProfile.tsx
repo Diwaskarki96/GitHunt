9f2106f6e1069f4623b08a1080d96b0764c5e189
Diwash <diwaskarki96@gmail.com>
2025-04-20 22:06:13 +0545

    graphql integrated

--- src/components/user/UserProfile.tsx
+++ src/components/user/UserProfile.tsx
@@ -1,10 +1,33 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
+import { UserData } from "../../types";
+import { GET_USER } from "../../queries";
 
 type UserProfileProps = {
   userName: string;
 };
 const UserProfile = ({ userName }: UserProfileProps) => {
-  return <h1 className="text-2xl font-bold">{userName}</h1>;
+  const { data, loading, error } = useQuery<UserData>(GET_USER, {
+    variables: { login: userName },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <h2 className="text-xl">Error: {error.message}</h2>;
+  if (!data) return <h2 className="text-xl">No data found</h2>;
+  const {
+    avatarUrl,
+    bio,
+    followers,
+    following,
+    gists,
+    name,
+    repositories,
+    url,
+  } = data.user;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">{bio}</h1>
+    </div>
+  );
 };
 
 export default UserProfile;
