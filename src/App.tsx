10c3cbcd0fb31a066ac2f68b771c73c2cc2f997c
Diwash <diwaskarki96@gmail.com>
2025-04-21 23:21:30 +0545

    setting utils

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SearchForm from "./components/form/SearchForm";
 import UserProfile from "./components/user/UserProfile";
 
 const App = () => {
-  const [userName, setUserName] = useState("Diwas");
+  const [userName, setUserName] = useState("DiwasKarki96");
   return (
     <main className="mx-auto max-w-6xl px-8 py-20">
       <SearchForm userName={userName} setUserName={setUserName} />
---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+import { Repository } from "./types";
+
+/**
+ * Calculates the top 5 most forked repositories
+ * @param repositories Array of repository data from GitHub API
+ * @returns Array of objects containing repository names and their fork counts
+ * Example return: [{ repo: "react", count: 1000 }, { repo: "vue", count: 500 }]
+ */
+// export const calculateMostForkedRepos = (
+//   repositories: Repository[]
+// ): { repo: string; count: number }[] => {
+//   if (repositories.length === 0) return [];
+// };
