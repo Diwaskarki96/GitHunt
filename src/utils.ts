cd83df421f8571c120b0a742ddeb78368fb32031
Diwash <diwaskarki96@gmail.com>
2025-04-22 21:32:26 +0545

    calculateMostForkedRepos

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,31 @@
 import { Repository } from "./types";
 
+export interface ForkedRepo {
+  repo: string;
+  count: number;
+}
+
 /**
  * Calculates the top 5 most forked repositories
  * @param repositories Array of repository data from GitHub API
  * @returns Array of objects containing repository names and their fork counts
  * Example return: [{ repo: "react", count: 1000 }, { repo: "vue", count: 500 }]
  */
-// export const calculateMostForkedRepos = (
-//   repositories: Repository[]
-// ): { repo: string; count: number }[] => {
-//   if (repositories.length === 0) return [];
-// };
+
+export const calculateMostForkedRepos = (
+  repositories: Repository[]
+): { repo: string; count: number }[] => {
+  if (repositories.length === 0) {
+    return [];
+  }
+  const forkedRepos = repositories
+    .map((repo) => {
+      return {
+        repo: repo.name,
+        count: repo.forkCount,
+      };
+    })
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 5);
+  return forkedRepos;
+};
